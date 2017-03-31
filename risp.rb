0b60c222927018a72f8ef8ada07c38c6dd43edc0
chikuwait <chikuwa.1703@gmail.com>
2017-03-31 22:17:38 +0900

    add token analyze

--- risp.rb
+++ risp.rb
@@ -2,4 +2,27 @@ def token(str)
   raise ArgumentError,'There is no argument' if str == nil
   str.gsub(/[()]/, ' \0 ').split
 end
+
+def read(tokens)
+  raise SytanxError 'unexpected EOF while reading' if tokens.length == 0
+  case token = tokens.shift
+
+  when '('
+    ary = []
+    until tokens[0] == ')'
+      ary.push read(tokens)
+    end
+    tokens.shift
+    ary
+  when ')'
+    raise SyntaxError, 'unexpected )'
+  else
+    atom(token)
+  end
+end
+
+def atom(token)
+  #something
+end
+
 puts token(ARGV[0])
